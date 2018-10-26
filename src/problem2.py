f8a5ccce4b5e686c9d2b2d0ce85bac0ab2283d19
scottjm1 <jasminescott2018@gmail.com>
2018-10-26 15:56:35 -0400

    Problem #2

--- src/problem2.py
+++ src/problem2.py
@@ -2,12 +2,13 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Jasmine Scott.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # COMPLETED: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
+import math
 
 
 def main():
@@ -90,10 +91,15 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # COMPLETED: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    index_of_max = 0
+    for k in range(len(sequence)):
+        if math.fabs(sequence[index_of_max]) < math.fabs(sequence[k]):
+            index_of_max = k
+    return index_of_max
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
