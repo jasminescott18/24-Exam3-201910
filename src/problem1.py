01500a9ec6e8a81906fdc288548927a3760010f4
scottjm1 <jasminescott2018@gmail.com>
2018-10-26 15:53:14 -0400

    Problem #1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Exam 3, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and Jasmine Scott.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # COMPLETED: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -85,8 +85,15 @@ def problem1(numbers):
     Type hints:
       :type numbers:  [int]
     """
+    divisible = len(numbers) // 3
+    total = 0
+    for k in range(divisible):
+        total = total + numbers[k + divisible]
+    return total
+
+
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # COMPLETED: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
